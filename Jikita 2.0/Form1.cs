4b7f7b20e21b0e606dbd54e9a1bd28f4cb282601
Popov and Hamitov <PopovAndHamitov@gmail.com>
2022-05-16 09:29:41 +0500

    Добавьте файлы проекта.

---
+++ Jikita 2.0/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jikita_2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            double rub = double.Parse(text);
+            double rubs = rub / 66.5;
+            string result = Convert.ToString(rubs);
+            listBox1.Items.Add(result);
+        }
+    }
+}
